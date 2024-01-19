c8404acc95eee92ae134647d5af46c09f1afdd14
c0np4nn4 <c0np4nn4@gmail.com>
2024-01-19 22:25:00 +0900

    done: `1_connect_wallet`

--- 1_connect_wallet/src/App.tsx
+++ 1_connect_wallet/src/App.tsx
@@ -1,46 +1,140 @@
-
-import React, { FC, useMemo } from "react";
-import { WalletProvider } from "@demox-labs/aleo-wallet-adapter-react";
-import { WalletModalProvider, WalletMultiButton } from "@demox-labs/aleo-wallet-adapter-reactui";
+import React, { FC, useEffect, useMemo, useState } from "react";
+import {
+  useWallet,
+  WalletProvider,
+} from "@demox-labs/aleo-wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@demox-labs/aleo-wallet-adapter-reactui";
 import { LeoWalletAdapter } from "@demox-labs/aleo-wallet-adapter-leo";
-import { DecryptPermission, WalletAdapterNetwork } from "@demox-labs/aleo-wallet-adapter-base";
+import {
+  DecryptPermission,
+  Transaction,
+  Transition,
+  WalletAdapterNetwork,
+  AleoTransaction,
+} from "@demox-labs/aleo-wallet-adapter-base";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button, Form } from "react-bootstrap";
 
 // Default styles that can be overridden by your app
 require("@demox-labs/aleo-wallet-adapter-reactui/styles.css");
 
-export const App: FC = () => {
+export const App = () => {
   const wallets = useMemo(
     () => [
       new LeoWalletAdapter({
         appName: "Leo Demo App",
       }),
     ],
-    [],
+    []
   );
 
   return (
-    <WalletProvider
-      wallets={wallets}
-      decryptPermission={DecryptPermission.UponRequest}
-      network={WalletAdapterNetwork.Testnet}
-      autoConnect
-    >
-      <WalletModalProvider>
-
-      <WalletMultiButton />
-
-
     <div className="container d-flex justify-content-center align-items-center vh-100">
-      <div className="rounded border-2 p-4 bg-light shadow">
-        <h1 className="text-center">Hello, World!</h1>
-        {/* Add other content or components inside the box */}
+      <div className="flex-column rounded border-2 p-4 bg-light shadow">
+        <h1 className="text-center">Hello, Aleo!</h1>
+
+        <WalletProvider
+          wallets={wallets}
+          decryptPermission={DecryptPermission.UponRequest}
+          network={WalletAdapterNetwork.Testnet}
+          autoConnect={true}
+        >
+          <WalletModalProvider>
+            <WalletMultiButton />
+            <WalletToolBox />
+          </WalletModalProvider>
+        </WalletProvider>
       </div>
     </div>
+  );
+};
 
-      </WalletModalProvider>
-    </WalletProvider>
+const WalletToolBox = () => {
+  const { wallet, connected } = useWallet();
+
+  // Now you can use the `wallet` object here
+
+  return (
+    <div> 
+      {connected ? (
+        <div className="mt-2 flex-column rounded border-2 p-4 bg-light shadow">
+          <TransferCredit />
+        </div>
+      ) : (
+        <div></div>
+      )}
+      {/* Your component code here */}
+    </div>
+  );
+};
+
+const TransferCredit = () => {
+  const { requestTransaction, publicKey } = useWallet();
+  const [amount, setAmount] = useState<number>(0);
+  const [fee, setFee] = useState<number>(0);
+  const [toAddr, setToAddr] = useState<string>("");
+
+  const handleTransfer = async () => {
+    
+    const transfer_public_transition = new Transition(
+      "credits.aleo",
+      "transfer_public",
+      [
+        toAddr, amount.toString() + "u64"
+      ]
+    ) 
+
+    const send_credit_tx = new Transaction(
+      publicKey!,
+      WalletAdapterNetwork.Testnet,
+      [transfer_public_transition],
+      fee,
+      false,
+    )
+
+    console.log("send_credit_tx:", send_credit_tx);
+
+    if (requestTransaction) {
+       const res = await requestTransaction(send_credit_tx);
+      setAmount(0);
+      setToAddr("");
+
+       console.log("check: ", res);
+    }
+  };
+
+  return (
+    <div>
+      <Form.Group>
+        <Form.Label>To Address:</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter recipient's address"
+          onChange={(e) => setToAddr(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Amount:</Form.Label>
+        <Form.Control
+          type="number"
+          placeholder="1,000,000 = 1 Aleo"
+          onChange={(e) => setAmount(Number(e.target.value))}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Fee:</Form.Label>
+        <Form.Control
+          type="number"
+          placeholder="1,000,000 = 1 Aleo"
+          onChange={(e) => setFee(Number(e.target.value))}
+        />
+      </Form.Group>
+      <Button onClick={handleTransfer}>Transfer Aleo Credits</Button>
+    </div>
   );
 };
 
